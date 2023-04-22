5d275fe98e576394d5df15de32fa0b6667927370
Arnab Sarkar <arnabsarkar013@gmail.com>
2023-04-22 12:28:10 +0530

    Update anharmonic_calibrated.m

--- figure_2/Figure_2CEFGH/anharmonic_calibrated.m
+++ figure_2/Figure_2CEFGH/anharmonic_calibrated.m
@@ -1,12 +1,9 @@
 clear all
 m=1;
-
-
   ki=10;kf=110;
  a=[];
  a1=[];
  a2=[];
-% a=zeros
  for k=ki:kf
 
 s=strcat('anharmonic_D1\grap_',num2str(k),'.csv'); %file path
@@ -25,13 +22,12 @@ end
 a=10.^((a-10.*log10(20))/10)./300;
 a1=10.^((a1-10.*log10(20))/10)./300;
 a2=10.^((a2-10.*log10(20))/10)./300;
-% a=sqrt(a./(8.5984*1e16));
 a=sqrt(a./(3.8824*10^(11)));
 a1=sqrt(a1./(3.8824*10^(11)));
 a2=sqrt(a2./(3.8824*10^(11)));
 
 
-  gate=[10:-0.1:0]
+gate=[10:-0.1:0]
 
 figure  
 imagesc(freq,gate,a');
